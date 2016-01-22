4c645522fa46fb21b311ad03b3f94a22fc0f4105
Peter Schulz <schulzp@users.noreply.github.com>
2016-01-22 17:43:22 +0100

    create search indices

--- core/src/main/java/edu/crimpbit/config/MongoConfiguration.java
+++ core/src/main/java/edu/crimpbit/config/MongoConfiguration.java
@@ -3,6 +3,8 @@ package edu.crimpbit.config;
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
+import edu.crimpbit.Recording;
+import edu.crimpbit.Subject;
 import edu.crimpbit.converter.StringToGesture;
 import edu.crimpbit.converter.StringToSubject;
 import edu.crimpbit.repository.RepositoryProvider;
@@ -12,10 +14,12 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.event.EventListener;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.convert.CustomConversions;
+import org.springframework.data.mongodb.core.index.Index;
 import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
 import org.springframework.data.mongodb.core.mapping.event.AfterDeleteEvent;
 import org.springframework.data.mongodb.core.mapping.event.AfterSaveEvent;
@@ -54,6 +58,19 @@ public class MongoConfiguration extends AbstractMongoConfiguration {
     @Override
     public CustomConversions customConversions() {
         return new CustomConversions(Arrays.asList(StringToSubject.INSTANCE, StringToGesture.INSTANCE));
+    }
+
+    @Override
+    public MongoTemplate mongoTemplate() throws Exception {
+        MongoTemplate mongoTemplate = super.mongoTemplate();
+        createIndices(mongoTemplate);
+        return mongoTemplate;
+    }
+
+    private void createIndices(MongoTemplate mongoTemplate) {
+        mongoTemplate.indexOps(Subject.class).ensureIndex(new Index().on("name", Sort.Direction.ASC).unique());
+        mongoTemplate.indexOps(Recording.class).ensureIndex(new Index().on("tags", Sort.Direction.ASC));
+        mongoTemplate.indexOps(Recording.class).ensureIndex(new Index().on("gesture", Sort.Direction.ASC));
     }
 
     @Bean
